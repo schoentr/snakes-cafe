f29772403c994e20d0d979d6e5ab3d13ee4c5486
tim schoen <schoentr@laptop-trs.localdomain>
2019-02-18 14:22:28 -0800

    totals are updating

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -1,7 +1,7 @@
 appetizers = ['Nachos', 'Wings', 'Fries']
 entrees = ['Salmon', 'Pasta', 'Cheeseburger', 'Salad']
 desserts = ['Ice Cream', 'Cake', 'Pie']
-drinks = ['Soda','Coffee','Water']
+drinks = ['Soda', 'Coffee', 'Water']
 menu = f"""
 **************************************
 **    Welcome to the Snakes Cafe!   **
@@ -12,27 +12,55 @@ menu = f"""
 
 Appetizers
 ----------
-Wings
-Cookies
-Spring Rolls
+{appetizers[0]}
+{appetizers[1]}
+{appetizers[2]}
 
 Entrees
 -------
-Salmon
-Steak
-Meat Tornado
-A Literal Garden
+{entrees[0]}
+{entrees[1]}
+{entrees[2]}
 
 Desserts
 --------
-Ice Cream
-Cake
-Pie
+{desserts[0]}
+{desserts[1]}
+{desserts[2]}
 
 Drinks
 ------
 {drinks[0]}
 {drinks[1]}
-{drinks[2]} """
-
+{drinks[2]} 
+"""
 print(menu)
+order_prompt = """
+***********************************
+** What would you like to order? **
+***********************************
+"""
+item_totals = {}
+for i in appetizers:
+    item_totals[i.lower()] = 0
+for i in entrees:
+    item_totals[i.lower()] = 0
+for i in desserts:
+    item_totals[i.lower()] = 0
+for i in drinks:
+    item_totals[i.lower()] = 0
+
+order_msg = ''
+item = ''
+# print(item_totals)
+while item != 'quit':
+    item = input(order_prompt).lower()
+    num = item_totals[item]
+    num += 1
+    item_totals[item] = num
+    if (item_totals[item] == 1):
+        order_msg = f'**   You have 1 order of {item}.   **'
+    else:
+        order_msg = f'**   You have {num} order of {item}.   **'
+    
+    print(order_msg)
